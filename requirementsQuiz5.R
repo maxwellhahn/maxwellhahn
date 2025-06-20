de5d53d87d88fa9562ea6cbd476699d79ff4b704
Maxwell Hahn <mjh6831@psu.edu>
2025-06-20 09:31:57 -0400

    Add files via upload

---
+++ requirementsQuiz5.R
@@ -0,0 +1,14 @@
+packages <- c("ggplot2", "dplyr", "tidyr", 
+              "devtools", "mosaic", "rmarkdown",
+              "knitr","readr","rvest")
+missing <- packages[!(packages %in% installed.packages()[,"Package"])]
+if (length(missing)) install.packages(missing)
+invisible(lapply(packages, library, character.only = TRUE))
+if ("dcData" %in% installed.packages()){
+  library(dcData)
+} else {
+  devtools::install_github("mdbeckman/dcData")
+  library(dcData)
+}
+
+
